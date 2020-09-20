a8506f035035a5c90a5d70a556af5061e636e08c
Taurus Colvin <taurus.colvin@gmail.com>
2020-09-20 00:21:30 -0400

    teach app how do synth in the new way

--- bin/cdk-pipeline.ts
+++ bin/cdk-pipeline.ts
@@ -1,7 +1,12 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { CdkpipelinesDemoPipelineStack } from '../lib/pipeline';
+import 'source-map-support/register'
+import { App } from '@aws-cdk/core'
+import { CdkpipelinesDemoPipelineStack } from '../lib/pipeline'
 
-const app = new cdk.App();
-new CdkpipelinesDemoPipelineStack(app, 'CdkpipelinesDemoPipelineStack');
+const app = new App()
+
+new CdkpipelinesDemoPipelineStack(app, 'CdkpipelinesDemoPipelineStack', {
+  env: { account: 'taurus@money-team-dev', region: 'us-east-1' },
+})
+
+app.synth()
