0e1c3cf99dcd703b6b35c52717cabebbcc89e55d
Taurus Colvin <taurus.colvin@gmail.com>
2020-09-20 06:48:00 -0400

    document on-demand envs

--- bin/cdk-pipeline.ts
+++ bin/cdk-pipeline.ts
@@ -10,4 +10,14 @@ new CdkpipelinesDemoPipelineStack(app, 'CdkpipelinesDemoPipelineStack', {
   env: AppConfig.AWS,
 })
 
+// add your dev stacks here
+
+// new StageOne(app, 'MyTemporaryDevStack', {
+//   env: AppConfig.AWS,
+// })
+
+// and deploy to your isolated environment with:
+// npx cdk synth
+// npx cdk -a cdk.out/assembly-MyTemporaryDevStack deploy
+
 app.synth()
