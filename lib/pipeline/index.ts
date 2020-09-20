ba363ef580cd154d34910281ee5f50284814fd68
Taurus Colvin <taurus.colvin@gmail.com>
2020-09-20 01:32:44 -0400

    secret

--- lib/pipeline/index.ts
+++ lib/pipeline/index.ts
@@ -22,7 +22,7 @@ export class CdkpipelinesDemoPipelineStack extends Stack {
       sourceAction: new codepipelineActions.GitHubSourceAction({
         actionName: 'GitHub',
         output: sourceArtifact,
-        oauthToken: SecretValue.secretsManager('CDKGithubToken'),
+        oauthToken: SecretValue.secretsManager('github-token'),
         owner: 'colvint',
         repo: 'try-cdk-pipeline',
       }),
